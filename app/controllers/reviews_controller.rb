f68c1690989bc2c41e00a3508e207489f599645e
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-04-15 01:26:36 +0000

    Start adding css for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,6 +9,13 @@ class ReviewsController < ApplicationController
     redirect_to product_path(@product)
   end
 
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@product)
+  end
+
   private
 
   def review_params
