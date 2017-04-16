b72272fd01188f835a9a5d9b0351977e048fb327
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-04-16 00:09:26 +0000

    Add email uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 end
