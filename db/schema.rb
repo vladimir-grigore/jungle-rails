80723076bd01d1e4d097d950a0165944ae2d1066
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-04-17 19:04:58 +0000

    Install RSpec gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414194841) do
+ActiveRecord::Schema.define(version: 20170414202222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
